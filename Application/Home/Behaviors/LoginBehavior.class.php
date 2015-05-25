3d623a196cb13f963a85e8b6f44a683f4643e051
Dicky Wong <huangjw0124@163.com>
2015-05-25 23:51:15 +0800

    1.把autocode移动到common的function里面了 2。修改了部分V的地址引用

--- Application/Home/Behaviors/LoginBehavior.class.php
+++ Application/Home/Behaviors/LoginBehavior.class.php
@@ -4,10 +4,8 @@ namespace Home\Behaviors;
 
 class LoginBehavior extends \Think\Behavior {
 	public function run(&$param) {
-<<<<<<< HEAD
 		if (CONTROLLER_NAME != 'Login') {
-			import ( 'Org.Util.Authcode' );
-			$auth = (new \Org\Util\Atuhcode ())->authcode ( cookie ( 'auth' ) );
+			$auth = authcode ( cookie ( 'auth' ) );
 			if ($auth == '') {
 				redirect ( U ( 'Login/index' ) );
 			} else {
@@ -16,17 +14,5 @@ class LoginBehavior extends \Think\Behavior {
 					redirect ( U ( 'Login/index' ) );
 			}
 		}
-=======
-		// if(CONTROLLER_NAME!='Login'){
-		// import('Org.Util.Authcode');
-		// $auth=(new \Org\Util\Atuhcode())->authcode(cookie('auth'));
-		// if($auth==''){
-		// redirect(U('Login/index'));
-		// }else{
-		// $tmp=explode('\t',$auth);
-		// if($tmp[0]!=C('LOGIN_USERNAME')||$tmp[1]!=C('LOGIN_PASSWORD'))redirect(__APP__.'/Login');
-		// }
-		// }
->>>>>>> origin/master
 	}
 }
