ccdaab939429665c539efea5ae99a54cfad38e0d
Dicky Wong <huangjw0124@163.com>
2015-05-25 22:53:39 +0800

    简单修改

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,19 @@
+<?php
+return array (
+		'DB_TYPE' => 'sqlsrv',
+		'DB_DSN' => 'sqlsrv:Server=192.168.183.128;database=NewsPublishSystem',
+		'DB_USER' => 'gdut',
+		'DB_PWD' => '123',
+		'DB_PREFIX' => '',
+		'CACHE_NAVIGATION' => 86400, // 导航栏缓存时间
+		'CACHE_INDEX' => 300, // 首页新闻缓存时间
+		'CACHE_CATEGORY' => 60, // 具体分类下新闻列表缓存时间
+		'CACHE_PASSAGE' => 1800, // 每篇新闻缓存时间
+		'CACHE_SEARCH' => 3600, // 搜索结果缓存时间
+		'PASSAGE_LEN' => 30, // 新闻列表每页数目
+		'LOGIN_USERNAME' => 'gdutnews', // 登录用户名
+		'LOGIN_PASSWORD' => 'newsgdut', // 登录密码
+		'AUTH_KEY' => 'gdut!~@', // COOKIE密钥
+		'DEFAULT_FILTER' => 'htmlspecialchars,addslashes',
+		header ( "Content-Type: text/html; charset=utf-8" ) 
+); 
---
+++ Application/Home/Behaviors/LoginBehavior.class.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Home\Behaviors;
+
+class LoginBehavior extends \Think\Behavior {
+	public function run(&$param) {
+		if (CONTROLLER_NAME != 'Login') {
+			import ( 'Org.Util.Authcode' );
+			$auth = (new \Org\Util\Atuhcode ())->authcode ( cookie ( 'auth' ) );
+			if ($auth == '') {
+				redirect ( U ( 'Login/index' ) );
+			} else {
+				$tmp = explode ( '\t', $auth );
+				if ($tmp [0] != C ( 'LOGIN_USERNAME' ) || $tmp [1] != C ( 'LOGIN_PASSWORD' ))
+					redirect ( U ( 'Login/index' ) );
+			}
+		}
+	}
+}
