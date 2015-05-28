7a9162c58d659f2fff872d9c955e970c82c22240
Dicky Wong <huangjw0124@163.com>
2015-05-28 19:40:41 +0800

    主页模板

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,12 +4,12 @@ namespace Home\Controller;
 
 class IndexController extends \Think\Controller {
 	public function index() {
-		// 当日新闻加载
-		$this->assign ( 'notice', D ( 'Articles' )->getLatest ( 4 ) );
-		$this->assign ( 'announce', D ( 'Articles' )->getLatest ( 5 ) );
-		$this->assign ( 'note', D ( 'Articles' )->getLatest ( 6 ) );
-		// 导航栏分类加载
-		$this->assign ( 'category', D ( 'Categories' )->getCategory () );
+// 		// 当日新闻加载
+// 		$this->assign ( 'notice', D ( 'Articles' )->getLatest ( 4 ) );
+// 		$this->assign ( 'announce', D ( 'Articles' )->getLatest ( 5 ) );
+// 		$this->assign ( 'note', D ( 'Articles' )->getLatest ( 6 ) );
+// 		// 导航栏分类加载
+// 		$this->assign ( 'category', D ( 'Categories' )->getCategory () );
 		$this->display ( 'test' );
 	}
 }
