a5a1c757068c5c148b3c34e61a4bc63b54764bee
XiaoB <467360687@qq.com>
2015-05-30 01:01:38 +0800

    完成文章的浏览(差模板)

--- Application/Home/Controller/ArticleController.class.php
+++ Application/Home/Controller/ArticleController.class.php
@@ -2,6 +2,11 @@
 namespace Home\Controller;
 class ArticleController extends \Think\Controller {
 	public function index($id=0){
+		if($id!=0){
+			$data=D('Articles')->getPassage($id);
+			$this->assign('title',$data['title']);
+			$this->assign('data',$data);
+		}
 		$this->display();
 	}
 }
