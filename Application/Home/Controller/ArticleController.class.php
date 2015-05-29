a641960da6d1f44602cfd59054af3814938e4df4
XiaoB <467360687@qq.com>
2015-05-30 00:51:31 +0800

    修改标题

--- Application/Home/Controller/ArticleController.class.php
+++ Application/Home/Controller/ArticleController.class.php
@@ -2,6 +2,6 @@
 namespace Home\Controller;
 class ArticleController extends \Think\Controller {
 	public function index($id=0){
-
+		$this->display();
 	}
 }
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,6 +4,7 @@ namespace Home\Controller;
 
 class IndexController extends \Think\Controller {
 	public function index() {
+		$this->assign('title','广东工业大学新闻通知网');
  		// 最近新闻加载
 		$this->assign('data',array(
 			array('name'=>'最新通知','data'=>D ( 'Articles' )->getLatest ( 4,12 )),
