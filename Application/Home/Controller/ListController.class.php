ad6a238188ef2bef3952bb1ea75684c23a3717a8
XiaoB <467360687@qq.com>
2015-05-30 17:48:05 +0800

    弄好文章列表显示

--- Application/Home/Controller/ListController.class.php
+++ Application/Home/Controller/ListController.class.php
@@ -5,6 +5,11 @@ class ListController extends \Think\Controller {
 	public function index($category=0,$category2=0) {
 		$this->assign('category',$category);
 		$this->assign('category2',$category2);
+		$this->assign('data',D('Articles')->getTitle($category,$category2,1));
 		$this->display();
+	}
+
+	public function page($category=0,$category2=0,$page=1){
+		$this->ajaxReturn(D('Articles')->getTitle($category,$category2,$page));
 	}
 }
