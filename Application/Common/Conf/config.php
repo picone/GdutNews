d66729e70fd71b5d1c1893224410ce39ea871d44
Dicky Wong <huang@Dicky-PC>
2015-09-02 21:28:31 +0800

    测试环境配置

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,19 @@
+<?php
+return array (
+		'DB_TYPE' => 'sqlsrv',
+		'DB_DSN' => 'sqlsrv:Server=10.10.123.11;database=NewsPublishSystem',
+		'DB_USER' => 'gdut',
+		'DB_PWD' => '123',
+		'DB_PREFIX' => '',
+		'MULTI_MODULE' => false,
+		'URL_PARAMS_BIND_TYPE' => 1,
+		'CACHE_NAVIGATION' => 86400, // 导航栏缓存时间
+		'CACHE_INDEX' => 300, // 首页新闻缓存时间
+		'CACHE_CATEGORY' => 60, // 具体分类下新闻列表缓存时间
+		'CACHE_SEARCH' => 3600, // 搜索结果缓存时间
+		'PASSAGE_LEN' => 30, // 新闻列表每页数目
+		'LOGIN_USERNAME' => 'gdutnews', // 登录用户名
+		'LOGIN_PASSWORD' => 'newsgdut', // 登录密码
+		'AUTH_KEY' => 'gdut!~@', // COOKIE密钥
+		'DEFAULT_FILTER' => 'htmlspecialchars,addslashes' 
+);
