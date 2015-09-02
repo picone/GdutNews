76fa86e289757cb105b787c162df51e9e802de43
chien <467360687@qq.com>
2015-09-02 21:39:48 +0800

    登录模块

--- Application/Home/Behaviors/LoginBehavior.class.php
+++ Application/Home/Behaviors/LoginBehavior.class.php
@@ -5,13 +5,15 @@ namespace Home\Behaviors;
 class LoginBehavior extends \Think\Behavior {
 	public function run(&$param) {
 		if (CONTROLLER_NAME != 'Login') {
-			$auth = authcode ( cookie ( 'auth' ) );
+			$auth = authcode(cookie( 'auth' ));
 			if ($auth == '') {
-				redirect ( U ( 'Login/index' ) );
+                redirect(__APP__.'/Login');
 			} else {
-				$tmp = explode ( '\t', $auth );
-				if ($tmp [0] != C ( 'LOGIN_USERNAME' ) || $tmp [1] != C ( 'LOGIN_PASSWORD' ))
-					redirect ( U ( 'Login/index' ) );
+				list($username,$password)=explode( '\t',$auth);
+                $GLOBALS['username']=D('User')->login($username,$password);
+                if(!$GLOBALS['username']){
+                    redirect(__APP__.'/Login');
+                }
 			}
 		}
 	}
--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -12,13 +12,15 @@ class LoginController extends Controller {
 
 	public function login() {
 		if (IS_POST) {
-			if (I ( 'post.un' ) == C ( 'LOGIN_USERNAME' ) && I ( 'post.passwd' ) == C ( 'LOGIN_PASSWORD' )) {
+            if(D('User')->login(I('post.un'),I('post.passwd'))){
 				cookie ( 'auth', authcode ( implode ( '\t', array (
 						I ( 'post.un' ),
 						I ( 'post.passwd' ) 
 				) ), 'ENCODE' ), I ( 'post.remember', 0 ) == 0 ? 0 : 7776000 );
 				redirect ( __APP__ );
 			} else {
+                $this->assign('un',I('post.un'));
+                $this->assign('passwd',I('post.passwd'));
 				$this->assign ( 'output', '账号或密码错误!' );
 				$this->display ( 'index' );
 			}
