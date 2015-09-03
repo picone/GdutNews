cfc982dcf82032342ee95eaa63586ecae8f7b873
Dicky Wong <huang@Dicky-PC>
2015-09-03 20:08:49 +0800

    配置修改

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -3,8 +3,8 @@ return array (
 		// 数据库设置
 		'DB_TYPE' => 'sqlsrv',
 		'DB_DSN' => 'sqlsrv:Server=;database=NewsPublishSystem',
-		'DB_USER' => '',
-		'DB_PWD' => '',
+		'DB_USER' => 'gdut',
+		'DB_PWD' => '123',
 		// ThinkPHP设置
 		'MULTI_MODULE' => false,
 		'URL_PARAMS_BIND_TYPE' => 1,
