f0c98c77cf073ec4a899d642a570e91704aa279d
Dicky Wong <huang@Dicky-PC>
2015-11-21 02:10:22 +0800

    [修改] 文章正文内容标签属性过滤

--- Application/Home/Controller/ArticleController.class.php
+++ Application/Home/Controller/ArticleController.class.php
@@ -19,12 +19,17 @@ class ArticleController extends Controller {
 	private function formatData($content) {
 		if ($content != null) {
 			// 为表格添加响应式表格样式
-			$content = str_replace ( '<table', '<div class="table-responsive"><table class="table table-striped table-bordered table-hover"', $content );
-			$content = str_replace ( '</table>', '</table></div>', $content );
+			$content = preg_replace ( '/\<table/', '<div class="table-responsive"><table class="table table-striped table-bordered table-hover"', $content );
+			$content = preg_replace ( '/\<\/table\>/', '</table></div>', $content );
 			// 为图片添加超级链接在新窗口打开,并添加响应式图片样式
-			$content = preg_replace ( '/<img.*?src="(.*?)".*?>/', '<a href="${1}" target="_blank"><img src="${1}" alt="图片" class="img-responsive"/></a>', $content );
-            $content=preg_replace('/(&nbsp;){9,}/','',$content);
-            $content=preg_replace('/[ ]{9,}/','',$content);
+			$content = preg_replace ( '/\<img.*?src=\"(.*?)\".*?\>/', '<a href="${1}" target="_blank"><img src="${1}" alt="图片" class="img-responsive"/></a>', $content );
+			// 替换空格
+			$content = preg_replace ( '/[ ]{2,}/', '&nbsp;', $content );
+			// 删除大于8个的空格
+			$content = preg_replace ( '/(&nbsp;){9,}/', '', $content );
+			// 删除自定义字体大小
+			$content = preg_replace ( '/font\-size\:[ ]*\d+(px|em|pt|ex|pc|in|mm|cm)/', '', $content );
+			$content = preg_replace ( '/size\=\"\d+\"(?=.*?\>)/', '', $content );
 		}
 		return $content;
 	}
